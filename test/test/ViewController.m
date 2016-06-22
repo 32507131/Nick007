78a6109abd20b7bb0a673cdf2f7be2d706cb1a28
Nick007 <mac@hakunamatata.local>
2016-06-22 10:31:10 +0800

    Nick
    Change Value

--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()<UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout>
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
 
 @end
 
@@ -24,7 +24,7 @@
         [self.view addSubview:btn];
     }
     
-    [self createCollectionView];
+    [self createTableView];
 //    for (long long i=0; i<10000000000; i++) {
 //        NSString*string=@"Abc";
 //        string=[string lowercaseString];
@@ -40,69 +40,25 @@
     
 }
 
-/*创建CollectionView*/
-- (void)createCollectionView
-{
-    UICollectionViewFlowLayout * flowLayout = [[UICollectionViewFlowLayout alloc]init];
+#pragma mark---CreateTableView---
+-(void)createTableView{
+    UITableView*myTableView=[[UITableView alloc]initWithFrame:[UIScreen mainScreen].bounds style:UITableViewStyleGrouped];
+    myTableView.delegate=self;
+    myTableView.dataSource=self;
     
-    UICollectionView * collectionView = [[UICollectionView alloc]initWithFrame:[UIScreen mainScreen].bounds collectionViewLayout:flowLayout];
-    /*设置默认collectionView的背景颜色---白色*/
-    collectionView.backgroundColor = [UIColor whiteColor];
-    /*设置collectionView的代理*/
-    collectionView.delegate = self;
-    collectionView.dataSource = self;
-    /*添加collectionView到父视图*/
-    [self.view addSubview:collectionView];
-    /*注册重用cell*/
-    [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cell"];
-    /*注册重用区头区尾View*/
-    [collectionView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"header"];
-    [collectionView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:@"footer"];
+    [myTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    [self.view addSubview:myTableView];
 }
-
-#pragma mark - ----------------collectionView协议方法-----------------
-/*设置collectionView区数-默认为1*/
-- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
-{
-    NSLog(@"%s",__FUNCTION__);
-    return 1;
+#pragma mark---tableViewDelegate---
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return 10;
 }
-/*设置collectionView行数-默认为20*/
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
-{
-    return 20;
-}
-/*设置collectionView行内容*/
-- (UICollectionViewCell*)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
-{
-    UICollectionViewCell * cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
-    cell.backgroundColor =[UIColor colorWithRed:arc4random()%256/255.0 green:arc4random()%256/255.0 blue:arc4random()%256/255.0 alpha:1];
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell*cell=[tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    
     return cell;
 }
-/*设置collectionView区头内容*/
-- (UICollectionReusableView*)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
-{
-    NSLog(@"%s",__FUNCTION__);
-    if (kind == UICollectionElementKindSectionHeader) {
-        UICollectionReusableView * headerView = [collectionView dequeueReusableSupplementaryViewOfKind:kind withReuseIdentifier:@"header" forIndexPath:indexPath];
-        return headerView;
-    }else
-    {
-        UICollectionReusableView * footerView = [collectionView dequeueReusableSupplementaryViewOfKind:kind withReuseIdentifier:@"footer" forIndexPath:indexPath];
-        return footerView;
-    }
-}
-/*设置collectionView区头大小*/
-- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section
-{
-    return CGSizeMake(414, 300);
-}
-/*设置Cell距离边界尺寸*/
-- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
-{
-    return UIEdgeInsetsMake(10, 10, 10, 10);
-}
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
